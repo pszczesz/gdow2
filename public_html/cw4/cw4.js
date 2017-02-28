e9c7e45cc5627d446407416a8b23830da23ff804
prof <prof@Pracownia12>
2017-02-28 10:54:46 +0100

    first2

--- public_html/cw4/cw4.js
+++ public_html/cw4/cw4.js
@@ -1,2 +1,25 @@
 
-alert("Działam");
+window.onload = start;
+
+function start(){
+    var p1 = document.getElementById("first");
+    var p2 = document.getElementById("second");
+    p1.style.color = "black";
+    p2.style.fontWeight = "normal";
+    p1.onclick = klik;    
+    p2.onclick = klik2;
+}
+function klik(){
+    if(this.style.color==="black"){
+        this.style.color="red";
+    }else{
+        this.style.color = "black";
+    }
+}
+function klik2(){
+    if(this.style.fontWeight==="normal"){
+        this.style.fontWeight="bold";
+    }else{
+        this.style.fontWeight = "normal";
+    }
+}
