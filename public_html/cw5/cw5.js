cf6859e948328b66c8a4afde2c4eac8eeedc47ed
prof <prof@Pracownia12>
2017-02-28 11:21:21 +0100

    first2

--- public_html/cw5/cw5.js
+++ public_html/cw5/cw5.js
@@ -1,7 +1,14 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
+window.onload = start;
 
+function start(){
+    var btn = document.getElementById("zatwierdz");
+    btn.onclick = klik;
+}
+function klik(){
+    var imie = document.getElementById("imie").value;
+    var nazwisko = document.getElementById("nazwisko").value;
+    var pensja = parseInt(document.getElementById("pensja").value);
+    document.getElementById("wynik").innerHTML = "Dane: "
+            +imie+" "+nazwisko+" pensja: "+pensja;
+}
