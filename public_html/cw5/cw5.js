1853822f8c0035e0265413b373f0baf95acce3e8
prof <prof@Pracownia12>
2017-03-03 08:19:52 +0100

    first2

--- public_html/cw5/cw5.js
+++ public_html/cw5/cw5.js
@@ -9,6 +9,11 @@ function klik(){
     var imie = document.getElementById("imie").value;
     var nazwisko = document.getElementById("nazwisko").value;
     var pensja = parseInt(document.getElementById("pensja").value);
-    document.getElementById("wynik").innerHTML = "Dane: "
+    if(imie.trim()=="" || nazwisko.trim()=="" || isNaN(pensja)){
+        document.getElementById("wynik").innerHTML = "Błędne dane!!!";
+    }else{
+        document.getElementById("wynik").innerHTML = "Dane: "
             +imie+" "+nazwisko+" pensja: "+pensja;
+    }
+    
 }
