810c5ec7047556b023edad5f1e9c45219dda0c8f
prof <prof@Pracownia12>
2017-03-17 11:19:39 +0100

    first2

--- public_html/cw15/cw15.js
+++ public_html/cw15/cw15.js
@@ -1,2 +1,31 @@
+window.onload = start;
+function start() {
+    document.getElementById("oblicz").onclick = klik;
+}
+function klik() {
+    var temp = parseFloat(document.getElementById("temp").value);
+    if (isNaN(temp)) {
+        alert("Błędne dane!!");
+        document.getElementById("wynik").innerHTML = "";
+        return;
+    }
+    radios = document.getElementsByName("wyb");
+    for (var i = 0; i < radios.length; i++) {
+        if (radios[i].checked) {
+            var wyb = radios[i].value;
+            break;
+        }
+    }
+    var wynik = document.getElementById("wynik");
+    switch (wyb) {
+        case "cel":
+            wynik.innerHTML = (32 + 9 / 5 * temp) + " <sup>o</sup>F";
+            break;
+        case "far":
+            wynik.innerHTML = (5 / 9 * (temp - 32)) + " <sup>o</sup>C";
+            break;
+        default :
+    }
+}
 
 
