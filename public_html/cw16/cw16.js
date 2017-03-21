a6f979e7cca5feffd29c88c1d6f17a50013dbd16
prof <prof@Pracownia12>
2017-03-21 11:11:55 +0100

    first2

--- public_html/cw16/cw16.js
+++ public_html/cw16/cw16.js
@@ -1,7 +1,5 @@
 window.onload = start;
-
-
-function start(){
+function start(){   
     var wiosna =  [
         "green",
         "yellow",
@@ -13,7 +11,12 @@ function start(){
          "#ea794f",
          "#f7cb2c",
          "#80ed04" 
-    ];
-    //co 2s zmiana losowego koloru tła z tablicy wiosna dla obiektu body
+    ];    
+    setInterval(zmiana,3000);
+    function zmiana(){
+        // alert("dfgdfg");
+        var index = Math.floor(Math.random()*wiosna.length);
+        document.body.style.backgroundColor = wiosna[index];
+    }
 }
 
