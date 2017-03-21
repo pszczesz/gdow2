b617bc09b12d973353a73fb742f7a53265ed2a02
prof <prof@Pracownia12>
2017-03-21 09:20:34 +0100

    first2

---
+++ public_html/cw16/cw16.js
@@ -0,0 +1,19 @@
+window.onload = start;
+
+
+function start(){
+    var wiosna =  [
+        "green",
+        "yellow",
+        "purple",
+        "orange",
+         "red",
+         "#42f498",
+         "#d4eaaf",
+         "#ea794f",
+         "#f7cb2c",
+         "#80ed04" 
+    ];
+    //co 2s zmiana losowego koloru tła z tablicy wiosna dla obiektu body
+}
+
