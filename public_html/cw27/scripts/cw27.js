45d1ff6beac0c0962f72cdd82d63b0a95dc2f25d
prof <prof@Pracownia12>
2017-06-09 09:12:09 +0200

    first2

--- public_html/cw27/scripts/cw27.js
+++ public_html/cw27/scripts/cw27.js
@@ -1,7 +1,22 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
+$(document).ready(function () {
+    $(window).resize(function () {
+           // alert("resize");
+           $("#obiekt").css( { left: "0px", top: "0px" } )  ; 
+    });
+    $("#start").click(function () {
+        
+        var pos = $("#obiekt").position().left;
+        console.log($("#scene").width());
+        var sceneWidth = $("#scene").width(); 
+        var delta = "="+(sceneWidth-60);
+        if (pos < sceneWidth-200) {
+           
+            $("#obiekt").animate({left: "+"+delta}, 2000);
+        } else {            
+            $("#obiekt").animate({left: "-"+delta}, 2000);
+        }
 
+    });
+   
+});
