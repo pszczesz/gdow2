c068fb02fdbafde416e8cc258de66f637c94470a
pszczesz <pszczesz@poczta.onet.pl>
2017-06-09 09:29:11 +0200

    ini2

--- public_html/cw27/cw27.js
+++ public_html/cw27/cw27.js
@@ -3,4 +3,7 @@ $(document).ready(function (){
     $("dt").click(function (){
         $(this).next().toggle(500);
     });
+    $("h1").click(function(){
+        $(this).next().toggle(800);
+    });
 });
